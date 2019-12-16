b62fb7d16a3017d1f2f3b3161364256d83d6cde5
thoil <torbjonh@gmail.com>
2019-12-16 13:23:50 +0100

    some cleanup and things

---
+++ src/AppWrapper.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+const AppWrapper: React.FC = () => {
+    return <div>This is the shiny appwrapper</div>;
+};
+
+export default hot(module)(AppWrapper);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+// Deps
+import React, { useRef } from "react";
 import { render } from "react-dom";
-
-import styles from "./shiny.less";
+// Locals
+import AppWrapper from "./AppWrapper";
 
 // will initialize the wrapper that renders each app.
 const start = async () => {
     const Root = () => {
-        return <div className={styles.container}>THIS IS SHINY, RIGHT?</div>;
+        const overlay = useRef(null);
+        const bearcaveContext = {};
+
+        return (
+            <>
+                <AppWrapper />
+                <div id="overlay-root" ref={overlay}>
+                    {/* overlays, modals, notifications, tooltips and popovers */}
+                </div>
+            </>
+        );
     };
 
     render(<Root />, document.getElementById("root"));
