42dc887cd5d3306afd44771a46225bafc45896b7
Torbjørn Høiland <t_hoiland@hotmail.com>
2020-10-27 13:43:07 +0100

    added an empty todo app and moved a dependency

--- src/apps/todo/index.tsx
+++ src/apps/todo/index.tsx
@@ -1,27 +1,27 @@
 // deps
 import React from "react";
 import { Route, Routes } from "react-router";
-import { Link } from "react-router-dom";
 // locals
 import { registerCaveApp } from "@utils";
+import { Link } from "react-router-dom";
 
-function Trippin(): JSX.Element {
+function Todo(): JSX.Element {
     return (
         <div>
-            <h2>Trippin</h2>
+            <h2>TODO</h2>
             <nav>
                 <Link to="somewhere">somewhere</Link>
             </nav>
 
             <Routes>
-                <Route path="somewhere" element={<h2>Trippin' somewhere</h2>} />
+                <Route path="somewhere" element={<h2>TODO' somewhere</h2>} />
             </Routes>
         </div>
     );
 }
 
 registerCaveApp({
-    key: "trippin",
-    name: "Trippin",
-    render: Trippin,
+    key: "todo",
+    name: "Todo",
+    render: Todo,
 });
