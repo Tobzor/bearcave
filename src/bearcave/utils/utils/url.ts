7bbae103fca5054f26f5bd6cbcf1a843c00f3020
Torbjørn Høiland <t_hoiland@hotmail.com>
2020-10-30 10:22:15 +0100

    removed safari unsupported regex and a console log

--- src/bearcave/utils/utils/url.ts
+++ src/bearcave/utils/utils/url.ts
@@ -5,14 +5,17 @@ export function trimTrailingSlash(url: string): string {
 }
 
 export function sanitizedUrl(url: string): string {
-    return trimTrailingSlash(url.replace(/(?<=[^:\s])(\/+\/)/g, separator));
+    // something about lookahead regex that breaks in safari.
+    // return trimTrailingSlash(url.replace(/(?<=[^:\s])(\/+\/)/g, separator));
+    return trimTrailingSlash(url);
 }
 
 export function combineUrls(base: string, ...parts: string[]): string {
     return sanitizedUrl(
         [base]
             .concat(parts)
-            .filter((a) => !!a)
+            .filter((part) => !!part)
+            .map((part) => encodeURIComponent(part))
             .join(separator),
     );
 }
