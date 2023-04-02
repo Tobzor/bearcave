f72b08d47450796b821043589d70ad4f00570652
Torbjørn Nesbø Høiland <t_hoiland@hotmail.com>
2023-04-02 12:06:21 +0200

    manually declaring vite alias as tspaths plugin did something wrong

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(), 
-    react(), 
-  ],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@components": path.resolve(__dirname, "src/bearcave/components"),
+            "@utils": path.resolve(__dirname, "src/bearcave/utils"),
+            "@types": path.resolve(__dirname, "src/bearcave/types"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@css": path.resolve(
+                __dirname,
+                "src/bearcave/components/core/styles",
+            ),
+        },
+    },
+});
