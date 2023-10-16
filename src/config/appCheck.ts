e5970ee678573aa6b190a6f8854fdbab9c6ba878
Tobzor <t_hoiland@hotmail.com>
2023-10-16 21:16:46 +0200

    added secret debug token to github and using this during build step in preview deploys

--- src/config/appCheck.ts
+++ src/config/appCheck.ts
@@ -6,15 +6,23 @@ import {
 
 declare global {
     interface Window {
-        FIREBASE_APPCHECK_DEBUG_TOKEN: boolean;
+        FIREBASE_APPCHECK_DEBUG_TOKEN?: boolean | string;
     }
 }
 
+// PROD public key for app-check. Only used if we don't assign debug_token.
 const reCaptchaKey = "6LcSEKAoAAAAADm5tRdKbPMgCxtKuwAsHBxJevQs";
+
 export function createAppCheck(app: FirebaseApp) {
-    if (process.env.NODE_ENV !== "production") {
-        self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
-    }
+    // We set debug tokens if they exist in the current environment (only local and preview)
+    // SHOULD NOT EXIST IN PROD
+    self.FIREBASE_APPCHECK_DEBUG_TOKEN = import.meta.env.VITE_DEBUG_TOKEN;
+
+    console.log(
+        "DEBUG TOKEN ASSIGNMENT",
+        import.meta.env.VITE_DEBUG_TOKEN,
+        self.FIREBASE_APPCHECK_DEBUG_TOKEN,
+    );
 
     return initializeAppCheck(app, {
         provider: new ReCaptchaEnterpriseProvider(reCaptchaKey),
